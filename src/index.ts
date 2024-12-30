d0d03bb37ba7051a67058e4bccad0bdb478a8134
Paulo Guilherme Santiago <37718188+PauloMachine@users.noreply.github.com>
2024-12-30 02:00:11 -0300

    feat: add netlify

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,10 @@ connectDB();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://olin-web.netlify.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    res.status(204).end();
-  } else {
-    next();
-  }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
 });
 
 app.use(cors(corsOptions));
--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,6 +1,5 @@
 export const corsOptions = {
-  origin: "https://olin-web.netlify.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-
