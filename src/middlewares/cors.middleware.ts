fbb6158e9005c82413f38ed3dd1808e6cc42dfe6
Paulo Guilherme Santiago <37718188+PauloMachine@users.noreply.github.com>
2024-12-30 01:05:23 -0300

    feat: add cors

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,5 +1,5 @@
 export const corsOlin = {
-  origin: ["http://localhost:3000", "https://meu-frontend.com"],
+  origin: ["http://localhost:3000", "https://olin-web.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
