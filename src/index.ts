f488058d44bdaa52edff014025e8e679588ade7f
Paulo Guilherme Santiago <37718188+PauloMachine@users.noreply.github.com>
2024-12-30 01:30:23 -0300

    refactor: adjust cors

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ connectDB();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://olin-web.netlify.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
